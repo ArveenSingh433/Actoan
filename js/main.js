80e1dd7206d4a446e409a8b4cb492a28066e151f
ArveenSingh433 <135519878+ArveenSingh433@users.noreply.github.com>
2025-02-08 03:42:37 +0530

    adding two files over here

--- js/main.js
+++ js/main.js
@@ -73,6 +73,34 @@
         dotsData: true,
     });
 
+    jQuery(document).ready(function($) {
+        "use strict";
+        //  TESTIMONIALS CAROUSEL HOOK
+        $('#customers-testimonials').owlCarousel({
+            loop: true,
+            center: true,
+            items: 3,
+            margin: 0,
+            autoplay: true,
+            dots:true,
+            autoplayTimeout: 8500,
+            smartSpeed: 450,
+            responsive: {
+              0: {
+                items: 1
+              },
+              768: {
+                items: 2
+              },
+              1170: {
+                items: 3
+              }
+            }
+        });
+    });
+    
+    
+
 
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
@@ -87,4 +115,5 @@
     });
     
 })(jQuery);
+
 
